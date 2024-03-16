b469d909a3f3efc9d1db49e16e8d71c25e2f0454
moarifur <moarifur@gmail.com>
2024-03-16 21:42:34 +0600

    Building APIs: Getting Collection of Objects

--- app/api/users/route.jsx
+++ app/api/users/route.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
 import {NextResponse} from "next/server";
 
+/*------------------------ Getting a collection of objects -----------------------
+TODO: Fetch data from a db but here we hard coded data
+------------------------------------------------------------------------------ */
 export const GET = (req) => {
     return NextResponse.json([
         {id: 1, name: 'Mosh'},
