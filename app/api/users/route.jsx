c7b4ddacdd6984eabc09059a5a58c212ece50ec4
moarifur <moarifur@gmail.com>
2024-03-17 14:15:20 +0600

    Building APIs: Creating an Object

--- app/api/users/route.jsx
+++ app/api/users/route.jsx
@@ -9,3 +9,34 @@ export const GET = (req) => {
         {id: 2, name: 'Arif'},
     ])
 };
+
+/*----------------------------- Creating an object ------------------------------
+TODO-01:  To create an user, we need to -
+                (1) Send a request to the user endpoint
+                (2) Include an user object in the body of the request
+
+TODO-02:  Once we read the body of the request -
+                (1) Validate data
+                (2) If invalid data, return error code 400
+                (3) Else return the data has been created
+------------------------------------------------------------------------------ */
+// TODO-01: (1) Send a request to the user endpoint
+export async function POST(request) {
+    // TODO-01: (2) Include an user object in the body of the request
+    const body = await request.json()
+
+    // Reference: creating an object 01.png
+    // return NextResponse.json(body)
+    // Reference: creating an object 02.png
+    // return NextResponse.json({ id: 1, name: body.name })
+
+    // TODO-02: (1) Validate data
+    if (!body.name)
+        // TODO-02: (2) If invalid data, return error code 400
+        // Reference: creating an object 03.png
+        return NextResponse.json({ error: 'Name is required' }, {status: 400})
+    // TODO-02: (3) Else return the data has been created
+    // Reference: creating an object 04.png
+    return NextResponse.json({ id: 1, name: body.name }, {status: 201})
+}
+
